72f16b5951112c2a5742cb6b74fd34ca4c3e8658
Jianwei Cui <victor@jcui.info>
2015-01-24 00:35:40 -0500

    first commit

---
+++ WEB-INF/classes/src/FileUploadServlet.java
@@ -0,0 +1,200 @@
+/**
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ *
+ * You may not modify, use, reproduce, or distribute this software except in
+ * compliance with  the terms of the License at:
+ * http://java.net/projects/javaeetutorial/pages/BerkeleyLicense
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import com.facepp.http.*;
+import com.facepp.error.*;
+import org.json.JSONObject;
+/**
+ * File upload servlet example
+ */
+@WebServlet(name = "FileUploadServlet", urlPatterns = {"/upload"})
+@MultipartConfig
+public class FileUploadServlet extends HttpServlet {
+
+    private final static Logger LOGGER =
+            Logger.getLogger(FileUploadServlet.class.getCanonicalName());
+    private static final long serialVersionUID = 7908187011456392847L;
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+				
+	PrintWriter cout = response.getWriter();
+        cout.println("<h1>" + "Hi! I am CJW! Hello!" + "</h1>");
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+	Date date = new Date();
+	cout.println("<p> Current Time on Server: " + dateFormat.format(date) + "</p>"); //2014/08/06 15:59:48			
+				
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Create path components to save the file
+        //final String path = request.getParameter("destination");
+        final String path = "/opt/tomcat8/webapps/pic/";
+        final Part filePart = request.getPart("file");
+        final String fileName = dateFormat.format(date) + "_" +  getFileName(filePart);
+
+ 	final String PicURL = "http://jcui.info:8080/pic/" + fileName;	
+	
+	cout.println("<p> Your upload picture URL: " + PicURL + "</p>"); //2014/08/06 15:59:48			
+
+        OutputStream out = null;
+        InputStream filecontent = null;
+        final PrintWriter writer = response.getWriter();
+
+        try {
+            out = new FileOutputStream(new File(path + File.separator
+                    + fileName));
+            filecontent = filePart.getInputStream();
+
+            int read;
+            final byte[] bytes = new byte[1024];
+
+            while ((read = filecontent.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            writer.println("New file " + fileName + " created at " + path);
+            LOGGER.log(Level.INFO, "File {0} being uploaded to {1}",
+                    new Object[]{fileName, path});
+       	    cout.println("<p>" + "Picture Writing is done." + "</p>");
+
+       	cout.println("<p>" + "Picture Request is preparing." + "</p>");
+	String you_api_key_string = "abc0413d3e243c09eb516ae57260d0d3";
+	String you_api_secret_string = "XqX8sBJDCAJ6vQR6XI-86nZ_St6gkoZT";
+	HttpRequests httpRequests = new HttpRequests(you_api_key_string, you_api_secret_string);
+
+	PostParameters postParameters = new PostParameters().setUrl(PicURL).setAttribute("all");
+//	postParameters.getMultiPart().writeTo(cout);
+	try{
+		//httpRequests.request("detection", "detect", postParameters);
+        	cout.println("<p>" + "Picture Request is sent." + "</p>");
+		JSONObject result = httpRequests.detectionDetect(postParameters);
+		cout.println("<p> This is json " + result.toString() + "</p>");
+		
+	}
+	catch(FaceppParseException ex){
+		cout.println("Exception Caught!");
+		cout.println(ex.getErrorMessage());	
+	}
+	finally{
+
+	}	
+
+        } catch (FileNotFoundException fne) {
+            writer.println("You either did not specify a file to upload or are "
+                    + "trying to upload a file to a protected or nonexistent "
+                    + "location.");
+            writer.println("<br/> ERROR: " + fne.getMessage());
+
+            LOGGER.log(Level.SEVERE, "Problems during file upload. Error: {0}",
+                    new Object[]{fne.getMessage()});
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+            if (filecontent != null) {
+                filecontent.close();
+            }
+            if (writer != null) {
+                writer.close();
+            }
+        }
+	/*
+	int count = result.get("face").getCount();
+
+	String faceid = result.get("face").get(0).get("face_id").toString();
+
+	double confidence = result.get("face").get(0).get("").toString("attribute").get("gender").get("confidence").toDouble();
+
+
+	cout.println("<p> Count: " + count  + "</p>");
+	cout.println("<p> Face ID: " + faceid  + "</p>");
+	cout.println("<p> Confidence: " + confidence  + "</p>");
+	*/
+	 			
+    }
+
+    private String getFileName(final Part part) {
+        final String partHeader = part.getHeader("content-disposition");
+        LOGGER.log(Level.INFO, "Part Header = {0}", partHeader);
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Servlet that uploads files to a user-defined destination";
+    }
+}
